3cb4586284110141eff29f8a4d8c111c3f333c0f
octalpointtechnologies-ui <octalpointtechnologies@gmail.com>
2026-02-09 18:33:37 +0530

    Update new 1.js

--- new 1.js
+++ new 1.js
@@ -1,11 +1,12 @@
 function runProcess(course) {
     if (course === "MSW") {
         window.open(
-            "https://forms.office.com/r/9cEYBpGs2r",
+            "https://forms.office.com/Pages/ResponsePage.aspx?id=pT9MJPUur0SCtlIsp4LA9Pfzbic-KZlMnhwXNZ2xyl9UODJMSUNSQVVBRVJEVTBISE04VFFZWkVWMy4u",
             "_blank"
         );
     } else {
         console.log("Course received from main page:", course);
     }
 }
+
 

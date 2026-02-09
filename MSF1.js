20272e6b2b573975748a8f07804ddaadc96c6153
octalpointtechnologies-ui <octalpointtechnologies@gmail.com>
2026-02-10 00:09:18 +0530

    Create MSF1.js

---
+++ MSF1.js
@@ -0,0 +1,6 @@
+function runProcess(testType) {
+    window.open(
+        "https://forms.office.com/Pages/ResponsePage.aspx?id=pT9MJPUur0SCtlIsp4LA9Pfzbic-KZlMnhwXNZ2xyl9UODJMSUNSQVVBRVJEVTBISE04VFFZWkVWMy4u&embed=true",
+        "_blank"
+    );
+}

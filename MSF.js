cca6c18be86937478acc94855bb6df6a6e91b5dc
octalpointtechnologies-ui <octalpointtechnologies@gmail.com>
2026-02-10 00:14:43 +0530

    Update MSF.js

--- MSF.js
+++ MSF.js
@@ -1,5 +1,5 @@
 function runProcess(select.value) {
-    if (course === "Basic") {
+    if (select.value === "Basic") {
         window.open(
             "https://forms.office.com/Pages/ResponsePage.aspx?id=pT9MJPUur0SCtlIsp4LA9Pfzbic-KZlMnhwXNZ2xyl9URVRZMjVNTzAwTTVXV1ZZWExLU1dXT09BSy4u",
             "_blank"

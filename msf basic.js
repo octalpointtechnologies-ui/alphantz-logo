a20dc64c20dff74ad3a2df6f86caea46457dab04
octalpointtechnologies-ui <octalpointtechnologies@gmail.com>
2026-02-10 00:15:43 +0530

    Add files via upload

---
+++ msf basic.js
@@ -0,0 +1,10 @@
+function runProcess(select.value) {
+    if (select.value === "Basic") {
+        window.open(
+            "https://forms.office.com/Pages/ResponsePage.aspx?id=pT9MJPUur0SCtlIsp4LA9Pfzbic-KZlMnhwXNZ2xyl9URVRZMjVNTzAwTTVXV1ZZWExLU1dXT09BSy4u",
+            "_blank"
+        );
+    } else {
+        console.log("Course received:", course);
+    }
+}
